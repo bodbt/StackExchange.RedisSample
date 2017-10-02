767d1b1ef7fa62fffa68d043e163f5ff4c4a276a
王磊 <dexterw009@gmail.com>
2017-10-02 16:22:46 +0900

    add project

---
+++ RedisSample/RedisSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace RedisSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = @"C:\Program Files\Redis\redis-server.exe";
+            process.StartInfo = startInfo;
+
+            Console.WriteLine("Starting redis");
+            process.Start();
+
+            var redisHelper = new RedisHelper();
+
+            Console.WriteLine("Saving data in cache");
+            redisHelper.SaveStringData();
+
+            Console.WriteLine("Reading data from cache");
+            redisHelper.ReadStringData();
+
+            Console.ReadLine();
+
+            Console.WriteLine("Delete data from cache");
+            redisHelper.ClearData();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RedisSample/RedisSample/RedisHelper.cs
@@ -0,0 +1,58 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisSample
+{
+    public class RedisHelper
+    {
+        static RedisHelper()
+        {
+            RedisHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("localhost");
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        private int Count = 100;
+
+        public void SaveStringData()
+        {
+            var cache = Connection.GetDatabase();
+
+            var rnd = new Random();
+            for (int i = 1; i <= Count; i++)
+            {
+                var value = rnd.Next(0, Count);
+                cache.StringSet($"KEY_{i}", value);
+            }
+        }
+
+        public void ReadStringData()
+        {
+            var cache = Connection.GetDatabase();
+
+            for (int i = 1; i <= Count; i++)
+            {
+                var value = cache.StringGet($"KEY_{i}");
+                Console.WriteLine($"Value={value}");
+            }
+        }
+
+        public void ClearData()
+        {
+            var cache = Connection.GetDatabase();
+
+            cache.Execute("flushdb");
+        }
+    }
+}
